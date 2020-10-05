918d058a53bd14b1c62c3d40ac4b39e9834c077a
Abraham <allords@asu.edu>
2020-10-05 12:11:52 -0700

    Fixed the comments to Javadoc style

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -1,29 +1,53 @@
 package cse360assignment02;
 
-// Hey new add machine, who dis?
+/**
+ * @author Abraham Lords
+ * @version 1.1
+ */
 
 public class AddingMachine {
 	
 	private int total;
-	  
+
 	public AddingMachine () {
 	  total = 0;  // not needed - included for clarity
 	}
 	
+	/**
+	 * @param none
+	 * @return added total
+	 */
 	public int getTotal () {
 	  return 0;
 	}
 	
+	
+	/**
+	 * @param value value to be added
+	 * @return void
+	 */
 	public void add (int value) {
 	}
 	
+	/**
+	 * @param value value to be added
+	 * @return void
+	 */
 	public void subtract (int value) {
 	}
 	
+	/**
+	 * @param none
+	 * @return string
+	 */
 	public String toString () {
 	  return "";
 	}
 	
+	/**
+	 * @param none
+	 * @return void
+	 */
 	public void clear() {
 	}
 }

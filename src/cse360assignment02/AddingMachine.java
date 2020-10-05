f3231f780385e5e18a08a01f57467096f7568461
Abraham <allords@asu.edu>
2020-10-05 15:46:12 -0700

    version 2.0

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -2,52 +2,79 @@ package cse360assignment02;
 
 /**
  * @author Abraham Lords
- * @version 1.1
+ * @version 2.0
  */
 
 public class AddingMachine {
 	
 	private int total;
+	private String operations;
 
 	public AddingMachine () {
 	  total = 0;  // not needed - included for clarity
+	  operations = "0";
 	}
 	
 	/**
+	 * <p> gets total </p>
 	 * @param none
 	 * @return added total
 	 */
 	public int getTotal () {
-	  return 0;
+	  return total;
 	}
 	
-	
 	/**
-	 * @param value value to be added
+	 * <p> adds value to total </p>
+	 * @param value integer value to be added
 	 * @return void
 	 */
 	public void add (int value) {
+		total += value;
+		toString("+", value);
 	}
 	
 	/**
+	 * <p> subtracts value from total </p>
 	 * @param value value to be added
 	 * @return void
 	 */
 	public void subtract (int value) {
+		total -= value;
+		toString("-", value);
 	}
 	
 	/**
-	 * @param none
-	 * @return string
+	 * <p> adds operation to history </p>
+	 * @param operator mathematical operator to be added to operations string
+	 * @param value integer to be added to operations string
+	 * @return operations history of operations in string form
 	 */
-	public String toString () {
-	  return "";
+	public String toString(String operator, int value) {
+		operations += " ";
+		operations += operator;
+		operations += " ";
+		operations += value;
+		
+		return operations;
 	}
 	
 	/**
+	 * <p> prints history </p>
 	 * @param none
 	 * @return void
 	 */
+	public void printHistory() {
+		System.out.println(operations);
+	}
+	
+	/**
+	 * <p> clears operation history and reinitializes total </p>
+	 * @param none
+	 * @return void 
+	 */
 	public void clear() {
+		operations = "0";
+		total = 0;		
 	}
 }
---
+++ src/cse360assignment02/Main.java
@@ -0,0 +1,23 @@
+package cse360assignment02;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		AddingMachine myCalculator = new AddingMachine();
+		
+		myCalculator.add (4); 
+		myCalculator.subtract (2); 
+		myCalculator.add(5);
+		myCalculator.printHistory();
+		
+		myCalculator.clear();
+		myCalculator.printHistory();
+
+		myCalculator.add(5);
+		myCalculator.printHistory();
+
+
+	}
+
+}
